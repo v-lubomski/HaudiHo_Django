3e6230a7c28413c1e081835770ebd081aa5b2ce2
Vladislav <vlad.lubomski@gmail.com>
2019-12-30 23:36:45 +0400

    moved templates folder to project direcory

--- HaudiHo_Django/settings.py
+++ HaudiHo_Django/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'HaudiHo_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
